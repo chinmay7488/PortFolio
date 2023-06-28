a67b7c5235785af4be1129e07bfcc13ea08b28c9
Chinmay Jain <chinmayjain8314@gmail.com>
2023-06-28 20:21:07 +0530

    working on about me page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from portfolio.models import Skill, Certificate
 
 
 def HomePage(request):
     return render (request, 'home.html')
 
 def AboutPage(request):
-    return render (request, 'about.html')
+    queryset = Skill.objects.all().values()
+
+    sorted_queryset = queryset.order_by('Skill_priority').reverse()
+    #print(sorted_queryset[0])
+
+    return render (request, 'about.html',{'Skills':sorted_queryset})
 
 def ProjectPage(request):
     return render (request, 'project.html')
